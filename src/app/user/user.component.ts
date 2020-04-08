ca30a16548b2705eed87e231b5061e3e5a075f79
Yuliia Kucher <yuliia.s.kucher@gmail.com>
2020-04-08 19:35:05 +0300

    from lec 1 on angular

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,15 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {UserModel} from '../../models/UserModel';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent {
+
+  @Input()
+  user: UserModel;
+  constructor() { }
+
+}
